f392d5e432488c229da32d23787d0252eaaaf8a0
Charlie Underhill <charlie45@live.co.uk>
2019-11-16 15:53:41 +0000

    Altered structure of scss files, added new screenshot.png and added TODO's for main.js

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
+
 /**
  * TODO: Remove all the boilerplate code from blankslate
- * TODO: Build up the core folder
  * TODO: Setup helper functions that are always used
  * TODO: Setup theme to mirror a normal build
  * 		 - Setup posts/pages/loops correcly
@@ -9,62 +9,28 @@
  */
 
 // File Includes
+include_once('core/helper-functions.php');
+include_once('core/custom-post-types.php');
+include_once('core/wp-api-extend.php');
 
+function initial_theme_setup() {
 
-if ( ! function_exists( 'wordpress_base_theme_setup' ) ) :
-	/**
-	 * Sets up theme defaults and registers support for various WordPress features.
-	 *
-	 * Note that this function is hooked into the after_setup_theme hook, which
-	 * runs before the init hook. The init hook is too late for some features, such
-	 * as indicating support for post thumbnails.
-	 */
-	function wordpress_base_theme_setup() {
+	// Add theme support for title and featured image
+	add_theme_support('title-tag');
+	add_theme_support('post-thumbnails');
+	add_theme_support('html5', array(
+		'search-form',
+		'gallery',
+		'caption',
+	));
 
-		/*
-		 * Let WordPress manage the document title.
-		 * By adding theme support, we declare that this theme does not use a
-		 * hard-coded <title> tag in the document head, and expect WordPress to
-		 * provide it for us.
-		 */
-		add_theme_support( 'title-tag' );
-
-		/*
-		 * Enable support for Post Thumbnails on posts and pages.
-		 *
-		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-		 */
-		add_theme_support( 'post-thumbnails' );
-
-		// This theme uses wp_nav_menu() in one location.
-		register_nav_menus( array(
-			'menu-1' => esc_html__( 'Primary', 'wordpress-base-theme' ),
-		) );
-
-		/*
-		 * Switch default core markup for search form, comment form, and comments
-		 * to output valid HTML5.
-		 */
-		add_theme_support( 'html5', array(
-			'search-form',
-			'gallery',
-			'caption',
-		) );
-
-		// Add theme support for selective refresh for widgets.
-		add_theme_support( 'customize-selective-refresh-widgets' );
-
-		/**
-		 * Add support for core custom logo.
-		 *
-		 * @link https://codex.wordpress.org/Theme_Logo
-		 */
-		add_theme_support( 'custom-logo', array(
-			'height'      => 250,
-			'width'       => 250,
-			'flex-width'  => true,
-			'flex-height' => true,
-		) );
-	}
-endif;
-add_action( 'after_setup_theme', 'wordpress_base_theme_setup' );
+	// Setup theme menus
+	register_nav_menus(array(
+		'main-menu' => esc_html__('Main Menu', 'wordpress-base-theme'),
+		'footer-left' => esc_html__('Footer Left Menu', 'wordpress-base-theme'),
+		'footer-middle' => esc_html__('Footer Middle Menu', 'wordpress-base-theme'),
+		'footer-right' => esc_html__('Footer Right Menu', 'wordpress-base-theme'),
+	));
+	
+}
+add_action('after_setup_theme', 'initial_theme_setup');
